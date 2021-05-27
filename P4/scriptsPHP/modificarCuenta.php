6bb8c60fbbad887b0d39ac1d2c4109b4f5efcdfd
Leire Requena Garcia <leirereqgar@gmail.com>
2021-05-27 17:30:23 +0200

    Ya actualiza tus datos

--- P4/scriptsPHP/modificarCuenta.php
+++ P4/scriptsPHP/modificarCuenta.php
@@ -5,28 +5,30 @@
 	session_start();
 
 	$usuario = UsuariosBD::getUserByID($_SESSION['id_user']);
-	$valores = array ('intento' => true, 'correo' => $_POST['n_correo'],
-	                  'correo_valido' => false, 'nickname' => $_POST['n_nickname'],
+
+	$valores = array ('intento' => true, 'correo' => $_POST['correo'],
+	                  'correo_valido' => false, 'nickname' => $_POST['nickname'],
 	                  'nick_valido' => false);
 
-	if(!empty($valores['n_nickname'])){
-		$valores['nick_valido'] = UsuariosBD::verificarNickname($valores['n_nickname']);
+	if(!empty($valores['nickname'])){
+
+		$valores['nick_valido'] = UsuariosBD::verificarNickname($valores['nickname']);
 
 		if($valores['nick_valido'])
-			UsuariosBD::actualizarNickname($usuario['id_user'], $valores['n_nickname']);
+			UsuariosBD::actualizarNickname($usuario['id_user'], $valores['nickname']);
 	}
 
-	if(!empty($valores['n_correo'])){
-		$valores['correo_valido'] = UsuariosBD::verificarCorreo($valores['n_correo']);
+	if(!empty($valores['correo'])){
+		$valores['correo_valido'] = UsuariosBD::verificarCorreo($valores['correo']);
 
 		if($valores['correo_valido'])
-			UsuariosBD::actualizarNickname($usuario['id_user'], $valores['n_correo']);
+			UsuariosBD::actualizarNickname($usuario['id_user'], $valores['correo']);
 	}
 
-	if(!empty($_POST['n_password']))
-		UsuariosBD::actualizarPassword($usuario['id_user'], password_hash($_POST['n_password'], PASSWORD_BCRYPT));
+	if(!empty($_POST['password']))
+		UsuariosBD::actualizarPassword($usuario['id_user'], password_hash($_POST['password'], PASSWORD_BCRYPT));
 
-	//header("Location: ../miCuenta.php");
-	//exit();
+	header("Location: ../miCuenta.php");
+	exit();
 
 ?>
